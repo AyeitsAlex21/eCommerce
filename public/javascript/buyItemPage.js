da305f8494288905fa94c3944df8c9c9edd089c8
unknown <asummer8@uoregon.edu>
2024-03-09 15:48:20 -0800

    made stripe redirection for payments! all I would really have to do is save if successfull or not put in orders table in database

--- public/javascript/buyItemPage.js
+++ public/javascript/buyItemPage.js
@@ -1,7 +1,28 @@
 
 
+// This is your test publishable API key.
+const stripe = Stripe("pk_test_51OsAEeAw4cwCoIgTbEiMCEozfDmiaUiBf8kN8VyAd8v0JdtoEVHCTEqcUtQtNzdGZNQTCv4aViB9psKKF1TcCP4e00F8BhQEz4");
+
+//initialize();
+
+// Create a Checkout Session as soon as the page loads
+async function initialize() {
+    const response = await fetch("/itemInfo/create-checkout-session", {
+        method: "POST",
+    });
+
+    const { clientSecret } = await response.json();
+
+    const checkout = await stripe.initEmbeddedCheckout({
+        clientSecret,
+    });
+
+    // Mount Checkout
+    checkout.mount('#checkout');
+}
 
 document.addEventListener('DOMContentLoaded', async function() { 
+
     const urlParams = new URLSearchParams(window.location.search);
     const name = urlParams.get('name');
     const itemID = Number(urlParams.get('itemID'));
@@ -27,6 +48,8 @@ document.addEventListener('DOMContentLoaded', async function() {
         }
     }
 
+    
+
     fetch_item(itemID).then(item => {
         console.log(item.name);
 
@@ -40,10 +63,40 @@ document.addEventListener('DOMContentLoaded', async function() {
             <h3>${item.name}</h3>
             <p>${item.description}</p>
             <p>$${item.price}</p>
-            <button>BUY</button>
+            <button id="buy" onclick=purchase_item(${itemID}) >BUY
+            </button>
             `;
         }
     });
 
     
 });
+
+
+async function purchase_item(itemID) {
+    try {
+
+        const res = await fetch(`/itemInfo/create-checkout-session`, 
+        {
+            method: 'POST', 
+            headers: {
+              'Content-Type': 'application/json', // Specify the content type
+             
+            },
+            body: JSON.stringify({
+              data: itemID, // Your data here
+            }),
+        });
+
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status} instead of 200`);
+        }
+
+        const sessionData = await res.json();
+        window.location.href = sessionData.url;
+        
+    }
+    catch (error) {
+        console.error("Could not fetch the item requested", error);
+    }
+}
--- server/routes/buyItemPage.js
+++ server/routes/buyItemPage.js
@@ -2,6 +2,58 @@ const express = require("express");
 const { getItem } = require("../services/itemService.js");
 const buyItemRouter = express.Router()
 
+const stripe = require('stripe')(process.env.STRIPE_API_KEY);
+
+buyItemRouter.post('/create-checkout-session', async (req, res) => {
+  const itemID = Number(req.body.data);
+  const fetchedItem = await getItem(itemID);
+  
+  // Assuming `getItem` returns an object with a `price` property in the smallest currency unit (e.g., cents)
+  const price = Number(fetchedItem.price);
+  const pricedInCents = price * 100;
+
+  console.log("HEY")
+  
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: fetchedItem.name,
+          },
+          unit_amount: pricedInCents,
+        },
+        quantity: 1,
+      }],
+      mode: 'payment',
+      success_url: `https://${process.env.HOST}/return.html?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `https://${process.env.HOST}/cancel.html`
+    });
+    console.log(`About to redirect ${session.url}`)
+    res.json({url: session.url});
+
+  } catch (error) {
+    console.error('Error creating checkout session:', error);
+    res.status(500).send({error: error.message});
+  }
+});
+
+buyItemRouter.get('/session-status', async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
+  
+    res.json({
+      status: session.status,
+      customer_email: session.customer_details.email
+    });
+  } catch (error) {
+    console.error('Error retrieving session:', error);
+    res.status(500).send({error: error.message});
+  }
+});
+
 
 buyItemRouter.get("/getItem/:itemID", async (req, res, next) => {
 
